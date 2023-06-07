6b4f3dfd255ef7aa33f0fbe3e50db51890354c4d
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2023-06-07 18:20:29 +0800

    Upload HW1 part1 files

---
+++ School/ASU/CSE445/HW1/Part1/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HW1_Part1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string reverse(string str);
+
+        [OperationContract]
+        stringStatistic analyzeStr(string str);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class stringStatistic
+    {
+        public int uppercount;
+        public int lowercount;
+        public int digitcount;
+        public int vowelcount;
+
+
+        [DataMember]
+        public int uppercase
+        {
+            get { return uppercount; }
+            set { uppercount = value; }
+        }
+
+        [DataMember]
+        public int lowercase
+        {
+            get { return lowercount; }
+            set { lowercount = value; }
+        }
+
+        [DataMember]
+        public int digit
+        {
+            get { return digitcount; }
+            set { digitcount = value; }
+        }
+
+        [DataMember]
+        public int vowel
+        {
+            get { return vowelcount; }
+            set { vowelcount = value; }
+        }
+
+
+        public stringStatistic(int newuppercount, int newlowercount, int newdigitcount, int newvowelcount)
+        {
+            uppercase = newuppercount;
+            lowercase = newlowercount;
+            digit = newdigitcount;
+            vowel = newvowelcount;
+        }
+    }
+}
---
+++ School/ASU/CSE445/HW1/Part1/Service1.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HW1_Part1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string reverse(string str)
+        {
+            char[] cArray = str.ToCharArray();
+            Array.Reverse(cArray);
+            return new string(cArray);
+        }
+
+        public stringStatistic analyzeStr(string str)
+        {
+
+            int uppercount = 0;
+            int lowercount = 0;
+            int digitcount = 0;
+            int vowelcount = 0;
+
+            int uppercase()
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsUpper(str[i])) uppercount++;
+                }
+                return uppercount;
+            }
+            int lowercase()
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if(char.IsLower(str[i])) lowercount++;
+                }
+                return lowercount;
+            }
+            int digit()
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i])) digitcount++;
+                }
+                return digitcount;
+            }
+            int vowel()
+            {
+                string tempStr = str.ToLower();
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (tempStr[i] == 'a' || tempStr[i] == 'e' || tempStr[i] == 'i' || tempStr[i] == 'o' || tempStr[i] == 'u')
+                    {
+                        vowelcount++;
+                    }
+                }
+                return vowelcount;
+            }
+
+            uppercount = uppercase();
+            lowercount = lowercase();
+            digitcount = digit();
+            vowelcount = vowel();
+
+            stringStatistic aStr = new stringStatistic(uppercount, lowercount, digitcount, vowelcount);
+
+
+            return aStr;
+        }
+    }
+}
