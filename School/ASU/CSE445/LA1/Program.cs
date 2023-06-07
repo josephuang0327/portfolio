b360243f15398e15ac17c443b07dd6c6a3ebd0e9
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2023-06-07 18:48:01 +0800

    upload LA1 files

---
+++ School/ASU/CSE445/LA1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureActivity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.CountryInfoServiceSoapTypeClient myClient = new ServiceReference1.CountryInfoServiceSoapTypeClient("CountryInfoServiceSoap");
+
+            Console.WriteLine("Enter Country Name:");
+            string countryInput = Console.ReadLine();
+
+            Console.WriteLine("Result: \n");
+
+            string isocode = myClient.CountryISOCode(countryInput);
+            string countryName = myClient.CountryName(isocode);
+            string capital = myClient.CapitalCity(isocode);
+            var currency = myClient.CountryCurrency(isocode);
+            string isocurrency = currency.sISOCode;
+
+            Console.WriteLine(countryName);
+            Console.WriteLine(isocode);
+            Console.WriteLine(capital);
+            Console.WriteLine(isocurrency);
+
+
+        }
+    }
+}
