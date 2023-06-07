26a87837f874e582645eee33eaebec692c464105
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2023-06-07 18:49:54 +0800

    upload la4 files

---
+++ School/ASU/CSE445/LA4/Analyzer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading; // using namespace
+
+class Analyzer
+{
+	string analyzeStr;
+	public Analyzer(string str) { analyzeStr = str; }
+	
+	public void runDigit()
+    {
+		int count = 0;
+		for (int i = 0; i < analyzeStr.Length; i++)
+		{
+			if ((analyzeStr[i] >= '0') && (analyzeStr[i] <= '9'))
+			{
+				count++;
+			}
+		}
+		Console.WriteLine("Digits Count: " + count);
+	}
+	public void runUpper()
+	{
+		int count = 0;
+		for (int i = 0; i < analyzeStr.Length; i++)
+		{
+			if (char.IsUpper(analyzeStr[i]))
+			{
+				count++;
+			}
+		}
+		Console.WriteLine("Uppercase Count: " + count);
+	}
+	public void runPalin()
+	{
+		bool check = false;
+		char[] charArr = analyzeStr.ToCharArray();
+		Array.Reverse(charArr);
+		string revStr = new string(charArr);
+		if(analyzeStr.Equals(revStr))
+        {
+			check = true;
+        }
+		if(check)
+        {
+			Console.WriteLine("Palindrome Check: Is a Palindrom!" );
+		}
+        else
+        {
+			Console.WriteLine("Palindrome Check: Is NOT a Palindrom!");
+		}
+	}
+}
---
+++ School/ASU/CSE445/LA4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LectureActivity4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to analyze:");
+            string str = Console.ReadLine();
+            Analyzer analyzer = new Analyzer(str);
+
+            Thread digit = new Thread(new ThreadStart(analyzer.runDigit));
+            Thread upper = new Thread(new ThreadStart(analyzer.runUpper));
+            Thread palin = new Thread(new ThreadStart(analyzer.runPalin));
+
+            digit.Start();
+            upper.Start();
+            palin.Start();
+
+
+            digit.Join();
+            upper.Join();
+            palin.Join();
+
+            Console.WriteLine("Main Thread Completed");
+        }
+    }
+}
