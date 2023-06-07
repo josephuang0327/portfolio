8a58d34a7de64aafc5cc1ef159f02d844b0d8e46
josephuang0327 <55466018+josephuang0327@users.noreply.github.com>
2023-06-07 18:45:15 +0800

    upload hw4 part1 files

---
+++ School/ASU/CSE445/HW4/Part1/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Hw4.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" style="font-weight: 700" Text="XML Processor"></asp:Label>
+        </div>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="XML URL input: "></asp:Label>
+        <asp:TextBox ID="urlTextBox" runat="server"></asp:TextBox>
+        <asp:Button ID="ProcessButton" runat="server" OnClick="ProcessButton_Click" Text="Process" />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Processed XML:"></asp:Label>
+        <br />
+        <br />
+    </form>
+    <div id="div1" runat="server">
+    </div>
+</body>
+</html>
---
+++ School/ASU/CSE445/HW4/Part1/Default.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Web.UI.HtmlControls;
+
+namespace Hw4
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ProcessButton_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            
+            Label dynamicLabel = new Label();
+            div1.Controls.Add(dynamicLabel);
+
+            try
+            {
+                xmlDoc.Load(urlTextBox.Text);
+                XmlNodeList hotels = xmlDoc.GetElementsByTagName("Hotel");
+
+                for(int i = 0; i < hotels.Count; i++)
+                {
+                    //references from discord to add element dynamically
+                    HtmlGenericControl hotelDiv = new HtmlGenericControl("div");
+                    HtmlGenericControl names = new HtmlGenericControl("p");
+                    HtmlGenericControl stars = new HtmlGenericControl("p");
+                    HtmlGenericControl contact = new HtmlGenericControl("p");
+                    HtmlGenericControl phone = new HtmlGenericControl("p");
+                    HtmlGenericControl email = new HtmlGenericControl("p");
+                    HtmlGenericControl address = new HtmlGenericControl("p");
+                    HtmlGenericControl busline = new HtmlGenericControl("p");
+                    HtmlGenericControl spacing = new HtmlGenericControl("p");
+
+                    names.InnerText = "Hotel Name: " + hotels[i].ChildNodes[0].InnerText;
+                    stars.InnerText = "Stars: " + hotels[i].Attributes[0].InnerText;
+                    
+                    XmlNodeList contacts = xmlDoc.GetElementsByTagName("Contact");
+                    for(int j = 0; j < contacts.Count; j++)
+                    {
+                        phone.InnerText = "Phone: " + contacts[i].ChildNodes[0].InnerText;
+                        email.InnerText = "Email: " + contacts[i].ChildNodes[1].InnerText;
+                    }
+
+                    XmlNodeList addresses = xmlDoc.GetElementsByTagName("Address");
+                    for (int k = 0; k < addresses.Count; k++)
+                    {
+                        address.InnerText = "Address: " + addresses[i].ChildNodes[0].InnerText
+                                                         + addresses[i].ChildNodes[1].InnerText
+                                                         + addresses[i].ChildNodes[2].InnerText
+                                                         + addresses[i].ChildNodes[3].InnerText;
+                        XmlElement tempNode= xmlDoc.DocumentElement;
+                        if(hotels[i].ChildNodes[2].Attributes.Count == 1)
+                        {
+                            busline.InnerText = "Bus Lines: " + hotels[i].ChildNodes[2].Attributes[0].InnerText;
+                        }
+                        else
+                        {
+                            busline.InnerText = "Bus line not available";
+                        }
+
+                    }
+                    spacing.InnerText = "-----------------------------------------------";
+                    hotelDiv.Controls.Add(names);
+                    hotelDiv.Controls.Add(stars);
+                    hotelDiv.Controls.Add(contact);
+                    hotelDiv.Controls.Add(phone);
+                    hotelDiv.Controls.Add(email);
+                    hotelDiv.Controls.Add(address);
+                    hotelDiv.Controls.Add(busline);
+                    hotelDiv.Controls.Add(spacing);
+                    div1.Controls.Add(hotelDiv);
+                }
+            }
+            finally 
+            {
+                
+            }
+        }
+    }
+}
